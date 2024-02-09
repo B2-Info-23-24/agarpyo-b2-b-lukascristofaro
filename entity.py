46a8a0ba2b787481706a4d37225c55456055e7a7
lukas <lukas.cristofaro@ynov.com>
2024-02-09 22:39:23 +0100

    create classe entity

--- entity.py
+++ entity.py
@@ -4,12 +4,9 @@ import pygame
 class Entity(ABC):
     def __init__(self, size, position, color):
         self.size = size
-        self.position = position
         self.color = color
+        self.position = position
 
-    @abstractmethod
-    def update(self):
-        pass
     def draw(self, screen):
-        pygame.draw.circle(screen, self.color, (self.position[0], self.position[1]), 25)
+        pygame.draw.circle(screen, self.color, (self.position[0], self.position[1]), self.size)
 
